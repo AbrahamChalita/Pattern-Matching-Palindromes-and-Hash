bb34cfc35a01370c24063268dbade9ceff588e47
Abraham Chalita Núñez <54871116+AbrahamChalita@users.noreply.github.com>
2022-09-13 00:02:04 -0500

    Added comments to Part 1 - Z funtion

--- E1._Actividad Integradora_1/parte1.h
+++ E1._Actividad Integradora_1/parte1.h
@@ -16,25 +16,29 @@ class Parte1{
         Parte1(string transmission1, string transmission2, string mcode1, string mcode2, string mcode3);
 
         ~Parte1();
-
-
+        
+        // Definition of functions to be used
         void zArray(string texto, vector<int> &zArray);
         bool checkIfMatch(vector<int> zArray, int patLen);
         bool patternExists(string texto, string patron);
         void patternIndex(string texto, string patron);
         void printMatch(string texto, string patron);
-
-        vector<string> getCodes(string fileName);
         void searchPatterns(string transmission, string mcode1, string mcode2, string mcode3);
 };
 
-
+/* 
+Function used to create a Z array, given a string. The Z array is filled according to the matches of the given prefix.
+Time complexity: O(m + n)
+*/
 void zArray(string texto, vector<int> &zArray){
     int c;
+    // Initial values of the window are defined 
     int der = 0;  
     int izq = 0;
 
     for(int i = 0; i < texto.size(); i++){
+        // In this case, the index being bigger than r, the value is not yet known. The value might be not yet reached
+        // The unknown value (outside window boundaries) is calculated using the naive way, comparing one by one.
         if(i > der){
             izq = i;
             der = i;
@@ -42,17 +46,20 @@ void zArray(string texto, vector<int> &zArray){
             while(der < texto.size() && texto[der-izq] == texto[der]){
                 der++;
             }
-
+            // Corresponding value in Z[i] is assigned
             zArray[i] = der - izq;
             der--;
 
         }else{
-            // Se entra a la caja l ... r
+            // On the other hand, where i is less than r, it means that we are inside the window values, where we might know some of the values.
+            // c equals to the index where there is a match inside the window.
             c = i - izq;
-
+            
+            // If the Z value on index c is less that the remaining interval, the value z[i] is updated
             if(zArray[c] < der-i+1){
                 zArray[i] = zArray[c];
             }else{
+                // If there is no validation, values are calculated naively
                 izq = i;
                 while(der < texto.size()&& texto[der-izq] == texto[der]){
                     der++;
@@ -66,6 +73,8 @@ void zArray(string texto, vector<int> &zArray){
     }
 }
 
+// Function to see if a given array (Z array) has an existing pattern, looks if a value matches the pattern length
+// O(n)
 bool checkIfMatch(vector<int> zArray, int patLen){
     bool flag = false;
 
@@ -77,7 +86,8 @@ bool checkIfMatch(vector<int> zArray, int patLen){
 
     return flag;
 }
-
+// Function that creates a Z array, given a string and a pattern and validates wheather the pattern exists inside the string using the last function checkIfMatch()
+// O(m + n)
 bool patternExists(string texto, string patron){
     string nuevo = patron + '$' + texto;
 
@@ -91,6 +101,8 @@ bool patternExists(string texto, string patron){
     }
 }
 
+// Function that creates a Z array given a string and a pattern and prints, the positions or indexes where there is a match (value == pattern length)
+// O(m + n)
 void patternIndex(string texto, string patron){
     string nuevo = patron + '$' + texto;
 
@@ -104,17 +116,23 @@ void patternIndex(string texto, string patron){
     }
 }
 
+// Function that prints, in concatenation with the last function, when there is a match. The function prints the pattern and the line, given by a count as arrgument
+// along with the indexes
+// O(m + n)
 void printMatch(string texto, string patron, int count){
     cout << "(true) Found pattern: ";
     cout << "[" << patron << "]" << " on line: " << count - 1;
     patternIndex(texto, patron);
 }
 
-
+// Function that traverses a given vector of lines (transmission) within three other vectors of codes (patterns) to determine the positions where each element of codes
+// is present in the given transmission vector (linesTrans)
+// O(n^2)
 void searchPatterns(vector<string> linesTrans, vector<string> patternsCodeOne, vector<string> patternsCodeTwo, vector<string> patternsCodeThree){
     cout << "<---############## mcode1 ##############--->";
     cout << endl;
-
+    
+    // For each value of patternsCode, linesTrans is Travered entirely to see if there is a match within every element inside.
     for(int i = 0; i < patternsCodeOne.size(); i++){
         bool flag = false;
         for(int j = 0; j < linesTrans.size(); j++){
@@ -123,12 +141,13 @@ void searchPatterns(vector<string> linesTrans, vector<string> patternsCodeOne, v
                 flag = true;
             }
         }
-
+        // If not match was found given a pattern patterns[i] a message is shown
         if(flag == false){
             cout << "(false) Pattern: " << patternsCodeOne[i] << " NOT FOUND" << endl;
         }
     }
-
+    
+    // The traversal process is repeated three times, one for each patterns vector
     cout << endl;
     cout << "<---############## mcode2 ##############--->";
     cout << endl;
